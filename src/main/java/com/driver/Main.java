3d22891a21b722be0471fb46f864cf91692d1f12
salman-sam23 <salmansam3855@gmail.com>
2023-10-03 11:42:41 +0530

    Added my project

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        //Before getter and setter
+        //obj.name="Vinit";                     //Error-- The field RWOnly.name is not visible
+        //System.out.println(obj.name);    //Error-- The field RWOnly.name is not visible
+        
+        obj.setName("salman");
+        System.out.println(obj.getName());
+        
+    }
   
 }
